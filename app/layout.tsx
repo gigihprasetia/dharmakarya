f64fa272466d890baa04157d5ada5965536a9343
gigihprasetia <82494987+gigihprasetia@users.noreply.github.com>
2025-09-27 14:48:45 +0700

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,30 @@ export const metadata: Metadata = {
   title: "Kenteng Magic Dharma Karya - Body Repair Specialist | Yogyakarta",
   description:
     "Professional automotive body repair services in Tegal Mraen, Sleman, Yogyakarta. Expert dent repair, painting, and collision restoration.",
+  openGraph: {
+    title: "Kenteng Magic Dharma Karya",
+    description:
+      "Professional automotive body repair services in Tegal Mraen, Sleman, Yogyakarta. Expert dent repair, painting, and collision restoration.",
+    url: "https://dharmakarya.vercel.app",
+    siteName: "Kenteng Magic Dharma Karya",
+    images: [
+      {
+        url: "https://dharmakarya.vercel.app/16.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Kenteng Magic Dharma Karya",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Kenteng Magic Dharma Karya",
+    description:
+      "Professional automotive body repair services in Tegal Mraen, Sleman, Yogyakarta. Expert dent repair, painting, and collision restoration.",
+    images: ["https://dharmakarya.vercel.app/16.jpg"], // sama seperti di atas
+  },
 };
 
 export default function RootLayout({
